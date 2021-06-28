908829527ee9082b870de81a97911fd709da8e02
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-06-28 15:53:42 +0300

    Spaceship Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,17 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float forceMagnitude;
+    [SerializeField] private float maxVelocity;
+    
     private Camera _mainCamera;
+    private Rigidbody _rb;
+    private Vector3 _movementDirection;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
 
     private void Start()
     {
@@ -12,12 +22,26 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        if (!Touchscreen.current.primaryTouch.press.IsPressed()) return;
-        
-        var touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-        var worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
+        if (Touchscreen.current.primaryTouch.press.IsPressed())
+        {
+            var touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+            var worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
 
-        Debug.Log(touchPosition);
-        Debug.Log(worldPosition);
+            _movementDirection = transform.position - worldPosition;
+            _movementDirection.z = 0;
+            _movementDirection.Normalize();
+        }
+        else
+        {
+            _movementDirection = Vector3.zero;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_movementDirection == Vector3.zero) return;
+        
+        _rb.AddForce(_movementDirection * (forceMagnitude * Time.deltaTime), ForceMode.Force);
+        _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, maxVelocity);
     }
 }

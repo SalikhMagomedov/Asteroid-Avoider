d25a88890e5c6e42693a6376f49cfc64bd04d1ba
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-06-28 15:38:53 +0300

    Processing Input

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Camera _mainCamera;
+
+    private void Start()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (!Touchscreen.current.primaryTouch.press.IsPressed()) return;
+        
+        var touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+        var worldPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
+
+        Debug.Log(touchPosition);
+        Debug.Log(worldPosition);
+    }
+}

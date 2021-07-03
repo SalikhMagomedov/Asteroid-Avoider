a36adffdbc5b1b8bce4083ee1d3aaf8391b92457
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-07-03 11:37:09 +0300

    Asteroids

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var playerHealth = other.GetComponent<PlayerHealth>();
+
+        if (!playerHealth) return;
+
+        playerHealth.Crash();
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public void Crash()
+    {
+        gameObject.SetActive(false);
+    }
+}
